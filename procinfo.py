ec5058586c5e016cdb4a26fb9d4982cf2182dfe4
Srinaga-Mallipeddi <mallipeddisrinaga@gmail.com>
2025-05-19 16:19:49 -0700

    Done with Code modularization

--- procinfo.py
+++ procinfo.py
@@ -7,41 +7,68 @@ import typer
 
 app=typer.Typer()
 
+top_n_processes = {}
+proc_info = {}
 
 def get_top_process(count):
-    pass
+    print(f"Listing the top {count} running processes:")
+    for i, proc in enumerate(psutil.process_iter(['name', 'pid']), start=1):
+        if i > count:
+            break
+        top_n_processes.update({i: {'name': proc.info['name'], 'pid': proc.info['pid']}})
+        
 
-def get_proc_details(proc_name):
-    """
-    return  : dict  contains {"cpu": x,  "memory": y, "DU": z}
-    """
-    pass
+def get_proc_details():
+    selected_process = int(input("Select a process by number: "))
+    proc_info = {
+        "name": top_n_processes[selected_process]['name'],
+        "pid": top_n_processes[selected_process]['pid'],
+        "memory_MB": psutil.Process(top_n_processes[selected_process]['pid']).memory_info().rss / (1024 * 1024),
+        "cpu_percent": psutil.Process(top_n_processes[selected_process]['pid']).cpu_percent(interval=1),
+        "disk_io": psutil.Process(top_n_processes[selected_process]['pid']).io_counters()
+    }
+    return proc_info
 
-def display_proc_info(dict):
-    pass
+    
+
+def display_proc_info(proc_info):
+    if proc_info:
+        print(f"Memory consumption: {proc_info['memory_MB']} MB \n")
+        print(f"CPU Utilization: {proc_info['cpu_percent']}% \n")
+        print(f"Disk Utilization : {proc_info['disk_io']} \n")
+    
+
 
 def display_procs():
-    pass
+    for key, value in top_n_processes.items():
+        print(f"{key}: {value['name']}")
 
-def diplay_menu():
-    # showinng process on the console 
-    get_top_process()
+def diplay_menu(top_count):
+    get_top_process(top_count)
     display_procs()
-    # while loop logic 
-    get_proc_details()
-    display_proc_info
+    while True:
+        proc_info = get_proc_details()
+        display_proc_info(proc_info)
+        cont = input("Do you want to continue? (y/n): ").strip().lower()
+        if cont == 'y':
+            continue
+        elif cont == 'n':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid input. Please enter 'y' or 'n'.")
+    
 
 @app.command()             
-def main(platform: str = '', top_count: int = 5):
+def main(platform: str = typer.Option(..., help="Platform name (must be 'windows')"), top_count: int = 5):
      # Input validation : platform is mandatory arg
      # top_count is deafult arg 
     if platform.lower() != 'windows':
         print("This script is only for Windows platform")
-        return
+        raise SystemExit("Exiting...")
         # instaed of return exit the program :  
 
-    diplay_menu()
-
+    diplay_menu(top_count)
 
 
 
